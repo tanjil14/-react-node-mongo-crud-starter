a9bbf7c46f7e5c695c439b3174a57b0131092d47
tanjil <tanjil14@cse.pstu.ac.bd>
2021-10-27 09:24:11 +0600

    add user update

--- src/components/UpdateUser/UpdateUser.js
+++ src/components/UpdateUser/UpdateUser.js
@@ -1,11 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2>This is Update User</h2>
-        </div>
-    );
+  const [user, setUser] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, [id]);
+
+  //update user
+  const handleNameChange = (e) => {
+    const updatedName = e.target.value;
+    const updatedUser = { name: updatedName, email: user.email };
+    setUser(updatedUser);
+  };
+  const handleEmailChange = (e) => {
+    const updatedEmail = e.target.value;
+    const updatedUser = { name: user.name, email: updatedEmail };
+    setUser(updatedUser);
+  };
+  const handleUpdateUser = (e) => {
+    e.preventDefault();
+    const url = `http://localhost:5000/users/${id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+          if(data.modifiedCount){
+              alert('Successfully update.')
+          }
+      });
+  };
+  return (
+    <div>
+      <h2>Update :{user.name}</h2>
+      <p>
+        <small>{id}</small>
+      </p>
+      <form onSubmit={handleUpdateUser}>
+        <input
+          type="text"
+          onChange={handleNameChange}
+          value={user.name || ""}
+        />
+        <input
+          type="email"
+          onChange={handleEmailChange}
+          value={user.email || ""}
+        />
+        <input type="submit" value="Update" />
+      </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
